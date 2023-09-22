3dbe583a563172d1dbf4a7339ad61db0acaa9578
Emmanuel Adewoye <emmaxfans@gmail.com>
2023-09-22 18:45:19 +0100

    added the hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,33 @@
 import "./Hero.css";
+import purple_bg from "../../Assets/Icons/purple-lens-flare.svg";
+import star3 from "../../Assets/Icons/star3.svg";
+import star4 from "../../Assets/Icons/star4.svg";
+import vector4 from "../../Assets/Icons/vector4.svg";
+import Hackathon_guy from "../../Assets/Images/Hackathon-guy1.png";
 
 const Hero = () => {
-  return (
-    <>
-      <div>Hero</div>
-    </>
-  );
+	return (
+		<>
+			<div className="hero">
+				<div className="hero1">
+					<img src={star3} alt="big-idea" />
+					<img src={purple_bg} alt="purple-bg" />
+					<img src={star4} alt="big-idea" />
+				</div>
+				<div className="hero2">
+					<span>
+						<h3>Igniting a Revolution in HR Innovation</h3>
+
+						<img className="hero2-img" src={vector4} alt="vector-ellipse" />
+					</span>
+
+					<div className="hero2-sub">
+						<img src={Hackathon_guy} alt="big-idea" />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Hero;
--- src/Pages/Landing-page/Landing.jsx
+++ src/Pages/Landing-page/Landing.jsx
@@ -1,24 +1,24 @@
+import Body1 from "../../Components/Body/Body1";
 import Body2 from "../../Components/Body/Body2";
 import Faq from "../../Components/Faq/Faq";
 import Footer from "../../Components/Footer/Footer";
 import Hero from "../../Components/Hero/Hero";
-import Intro from "../../Components/Intro/Intro";
 import Nav from "../../Components/Navbar/Nav";
 import Privacy from "../../Components/Privacy-terms/Privacy";
 import "./Landing.css";
 
 const Landing = () => {
-  return (
-    <>
-      <Nav />
-      {/* <Hero /> */}
-      <Intro />
-      <Body2 />
-      {/* <Faq /> */}
-      {/* <Privacy /> */}
-      {/* <Footer /> */}
-    </>
-  );
+	return (
+		<>
+			<Nav />
+			<Hero />
+			<Body1 />
+			<Body2 />
+			{/* <Faq /> */}
+			{/* <Privacy /> */}
+			{/* <Footer /> */}
+		</>
+	);
 };
 
 export default Landing;

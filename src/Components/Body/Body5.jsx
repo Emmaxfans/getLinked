0a015d8b1e3f59f25593e8d1efb9eac1ba1052b2
Emmanuel Adewoye <emmaxfans@gmail.com>
2023-09-25 01:26:10 +0100

    updating the nav bar

---
+++ src/Components/Body/Body5.jsx
@@ -0,0 +1,11 @@
+import "./Body5";
+
+const Body5 = () => {
+	return (
+		<div>
+			<p>THIS IS BODY BODY 5</p>
+		</div>
+	);
+};
+
+export default Body5;
--- src/Components/Navbar/Nav.jsx
+++ src/Components/Navbar/Nav.jsx
@@ -5,42 +5,41 @@ import togglenav from "../../Assets/Icons/togglebtn.svg";
 import closebutton from "../../Assets/Icons/close.svg";
 
 const Nav = () => {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-    const getlinkedIcon = document.getElementById("getlinkedicon");
-    if (isOpen) {
-      getlinkedIcon.style.display = "block";
-    } else {
-      getlinkedIcon.style.display = "none";
-    }
-  };
+	const toggleNavbar = () => {
+		setIsOpen(!isOpen);
+		const getlinkedIcon = document.getElementById("getlinkedicon");
+		if (isOpen) {
+			getlinkedIcon.style.display = "block";
+		} else {
+			getlinkedIcon.style.display = "none";
+		}
+	};
 
-  return (
-    <div className="navbar-body">
-      <div className="navbar-container">
-        <div className={`navbar ${isOpen ? "responsive" : ""}`}>
-          <img id="getlinkedicon" src={getLinked} alt="getLinked" />
-
-          <img
-            src={isOpen ? closebutton : togglenav}
-            alt="nav-toggle"
-            className="icon"
-            onClick={toggleNavbar}
-          />
-
-          <a href="#">Timeline</a>
-          <a href="#"> Overview</a>
-          <a href="#">FAQs</a>
-          <a href="#">Contact</a>
-          <a href="#">
-            <button className="btn-nav">Register</button>
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+	// Inside Nav component
+	return (
+		<div className="navbar-body">
+			<div className="navbar-container">
+				<div className={`navbar ${isOpen ? "responsive" : ""}`}>
+					<img id="getlinkedicon" src={getLinked} alt="getLinked" />
+					<img
+						src={isOpen ? closebutton : togglenav}
+						alt="nav-toggle"
+						className="icon"
+						onClick={toggleNavbar}
+					/>
+					<a href="#">Timeline</a>
+					<a href="#">Overview</a>
+					<a href="#">FAQs</a>
+					<a href="#">Contact</a>
+					<a href="#">
+						<button className="btn-nav">Register</button>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Nav;
